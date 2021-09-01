a40e2286a04e9c3a44686ad09f708662038e1914
Kyle O'Callaghan <kyleocallaghan1@gmail.com>
2021-09-01 23:15:54 +0200

    Started to getKPAChart Generator started

--- core/static/assets/demo/demo.js
+++ core/static/assets/demo/demo.js
@@ -15,6 +15,114 @@ demo = {
     });
   },
 
+  generateDataSetsArray: function(data){
+    let dataSet;
+    for(var prop in data){
+      
+    }
+  },
+
+  generateLabelsArray: function(data){
+    let labels;
+    for(var prop in data){
+      labels.push(prop[Object.keys(prop[0])])
+    }
+    return labels;
+  },
+
+  getKPAChartData: function(){
+    gradientChartOptionsConfiguration = {
+      maintainAspectRatio: false,
+      legend: {
+        display: false
+      },
+      tooltips: {
+        bodySpacing: 4,
+        mode: "nearest",
+        intersect: 0,
+        position: "nearest",
+        xPadding: 10,
+        yPadding: 10,
+        caretPadding: 10
+      },
+      responsive: true,
+      scales: {
+        yAxes: [{
+          display: 0,
+          gridLines: 0,
+          ticks: {
+            display: false
+          },
+          gridLines: {
+            zeroLineColor: "transparent",
+            drawTicks: false,
+            display: false,
+            drawBorder: false
+          }
+        }],
+        xAxes: [{
+          display: 0,
+          gridLines: 0,
+          ticks: {
+            display: false
+          },
+          gridLines: {
+            zeroLineColor: "transparent",
+            drawTicks: false,
+            display: false,
+            drawBorder: false
+          }
+        }]
+      },
+      layout: {
+        padding: {
+          left: 0,
+          right: 0,
+          top: 15,
+          bottom: 15
+        }
+      }
+    };
+
+    gradientStroke = ctx.createLinearGradient(500, 0, 100, 0);
+    gradientStroke.addColorStop(0, '#80b6f4');
+    gradientStroke.addColorStop(1, chartColor);
+
+    gradientFill = ctx.createLinearGradient(0, 170, 0, 50);
+    gradientFill.addColorStop(0, "rgba(128, 182, 244, 0)");
+    gradientFill.addColorStop(1, "rgba(249, 99, 59, 0.40)");
+
+    let data = $.getJSON( "<insert url endpoint>", function( data ) {
+      return  {
+        labels: this.generateLabelsArray(data),
+        datasets: [{
+          label: "Data",
+          fill: true,
+          backgroundColor: gradientStroke,
+          borderColor: '#d048b6',
+          borderWidth: 2,
+          borderDash: [],
+          borderDashOffset: 0.0,
+          pointBackgroundColor: '#d048b6',
+          pointBorderColor:'rgba(255,255,255,0)',
+          pointHoverBackgroundColor: '#d048b6',
+          pointBorderWidth: 20,
+          pointHoverRadius: 4,
+          pointHoverBorderWidth: 15,
+          pointRadius: 4,
+          datasets: this.generateDataSetsArray(data),
+        }]
+      };
+    });
+
+    myChart = new Chart(ctx, {
+      type: 'line',
+      responsive: true,
+      data: data,
+      options: gradientChartOptionsConfiguration
+    });
+  },
+
   initDocChart: function() {
     chartColor = "#FFFFFF";
 
@@ -424,5 +532,5 @@ demo = {
       }
     });
   }
-
 };
+
